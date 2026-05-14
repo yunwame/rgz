aec139a4a3cbf76f6dd98dd22bdcd0731690ea52
Олеся Коваленко <yunwa.me@yandex.ru>
2026-05-14 21:10:15 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,98 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон o Дао и дэ',
+        publicationYear: 2022,
+        price: 590,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 1250,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 890,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 2021,
+        price: 720,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 2023,
+        price: 650,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Джейн Остин',
+        name: 'Гордость и предубеждение',
+        publicationYear: 2020,
+        price: 540,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Эрих Мария Ремарк',
+        name: 'Триумфальная арка',
+        publicationYear: 2019,
+        price: 780,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Юваль Ной Харари',
+        name: 'Sapiens. Краткая история человечества',
+        publicationYear: 2023,
+        price: 1100,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451 градус по Фаренгейту',
+        publicationYear: 2022,
+        price: 610,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 2024,
+        price: 430,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Фрэнсис Скотт Фицджеральд',
+        name: 'Великий Гэтсби',
+        publicationYear: 2021,
+        price: 580,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Анна Каренина',
+        publicationYear: 2020,
+        price: 890,
+        bestseller: false,
+        inStock: true
+    }
+];
